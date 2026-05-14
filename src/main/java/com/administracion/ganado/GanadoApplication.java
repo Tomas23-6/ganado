33de5d203957d413faabed6463acb032e4fe9470
Tomas23-6 <to.rodriguezp@duocuc.cl>
2026-05-14 16:07:48 -0400

    Primer commit

---
+++ src/main/java/com/administracion/ganado/GanadoApplication.java
@@ -0,0 +1,13 @@
+package com.administracion.ganado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GanadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/administracion/ganado/GanadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.administracion.ganado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GanadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
